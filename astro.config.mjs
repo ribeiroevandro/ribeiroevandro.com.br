bc977c50af8a404f737727c30913afabf7b84dda
Evandro Ribeiro <ribeiroevandro@live.com>
2024-01-07 04:37:50 -0300

    chore: test SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    server: {
+      https: true
+    }
+  }
 });
